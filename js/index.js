2c9613a191569f23d33f907cf0d42bf62aaaad28
guipizzini <Guido@Guidos-MacBook.local>
2016-12-08 01:23:25 +0100

    Update of the site buttons

--- js/index.js
+++ js/index.js
@@ -6,3 +6,12 @@ $(document).ready(function() {
         $(this).children('.rollover').fadeOut(250);
     });
 });
+
+$(document).ready(function() {
+    $('.rollover-regions').css('display', 'none');
+    $('.icons-regions').hover(function() {
+        $(this).children('.rollover-regions').fadeIn(100);
+    }, function() {
+        $(this).children('.rollover-regions').fadeOut(100);
+    });
+});
